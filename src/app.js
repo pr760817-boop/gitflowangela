e9407e39cf81644c9d95836834b45b33a55da579
unknown <luisfel200314@gmail.com>
2026-04-14 08:45:48 -0500

    Primer commit proyecto Felipe

---
+++ src/app.js
@@ -0,0 +1,23 @@
+export function saludar(nombre){
+  return "Hola " + nombre + ", bienvenido a mi aplicacion de Felipe!";
+}
+
+export function despedir(nombre){
+  return "Hasta pronto, " + nombre + "! Felipe";
+}
+
+export function estadoSistema(){
+  return {
+    estado: "activo",
+    version: "1.0.0",
+    message: "sistema felipe funcionando correctamente"
+  };
+}
+
+export function sumar(a, b){
+  return a + b;
+}
+
+export function restar(a, b){
+  return a - b;
+}
--- test/app.test.js
+++ test/app.test.js
@@ -1,26 +1,48 @@
-import {saludar, despedir, estadoSistema} from "../src/app.js"
+import {saludar, despedir, estadoSistema, sumar, restar} from "../src/app.js"
 
 function ejecutarPruebas() {
-let pasadas = 0;
-let fallidas = 0;
+  let pasadas = 0;
+  let fallidas = 0;
 
-const r1 = saludar("felipe")
-if (r1.includes("felipe")) {
-console.log ("Test 1 Pasado: funcion saludar en felipe es correcta")
-pasadas++;  
-} else {
-console.log("Test 1 Fallido: ", r1);
-fallidas++;
+  const r1 = saludar("felipe")
+  if (r1.includes("felipe")) {
+    console.log ("Test 1 Pasado: funcion saludar en felipe es correcta")
+    pasadas++;
+  } else {
+    console.log("Test 1 Fallido: ", r1);
+    fallidas++;
+  }
+
+  const actualEstado = estadoSistema();
+  if (actualEstado.estado === "activo"){
+    console.log ("Test 2 Pasado: estadoSistema() funciona en felipe");
+    pasadas++;
+  } else {
+    console.log ("Test 2 fallido: ", actualEstado, "en felipe");
+    fallidas++;
+  }
+
+  const r3 = sumar(5, 3);
+  if (r3 === 8) {
+    console.log("Test 3 Pasado: funcion sumar correcta");
+    pasadas++;
+  } else {
+    console.log("Test 3 Fallido:", r3);
+    fallidas++;
+  }
+
+  const r4 = restar(5, 3);
+  if (r4 === 2) {
+    console.log("Test 4 Pasado: funcion restar correcta");
+    pasadas++;
+  } else {
+    console.log("Test 4 Fallido:", r4);
+    fallidas++;
+  }
+
+  console.log("\nResultados: " + pasadas + " pasadas, " + fallidas + " fallidas");
+
+  if(fallidas > 0) process.exit(1);
 }
-const actualEstado = estadoSistema();
-if (actualEstado.estado === "activo"){
-console.log ("Test 2: Pasado: estadoSistemas() funciona en felipe");
-pasadas++;
-} else {
-console.log ("Test 2 fallido: ", estado, "en felipe");
-fallidas++;
-}
-console.log("\nResultados: " + pasadas + " pasadas, ", + fallidas + "fallidas");
-if(fallidas>0) process.exit(1);
-}
+
 ejecutarPruebas();
