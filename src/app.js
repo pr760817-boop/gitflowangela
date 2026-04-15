c6619e944c4d0958319e2d299d836b56c9c24041
ANGELA <PR760817@gmail.com>
2026-04-15 09:31:31 -0500

    modifique (api): version 1.1.0

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
-export function saludar (nombre){
-return "Hola " + nombre + ", bienvenido a aplicacion de Estiben Flow!" ;
+export function saludar(nombre) {
+  return "Hola " + nombre + ", bienvenido a aplicacion de Estiben Flow!";
 }
 
-export function despedir (nombre) {
-return "Hatsa pronto," + nombre + "¡Estiben Flow!";
+export function despedir(nombre) {
+  return "Hasta pronto, " + nombre + " ¡Estiben Flow!";
 }
+
 export function sumar(a, b) {
   return a + b;
 }
 
-export function estadoSistema () {
-return {
-estado: "activo",
-version : "1.0.0" ,
-message : "Sistema funcionando correctamente"
-};
+export function restar(a, b) {
+  return a - b;
 }
 
-export function healthCheck(){
-    return null;
+export function estadoSistema() {
+  return {
+    estado: "activo",
+    version: "1.0.0",
+    message: "Sistema funcionando correctamente"
+  };
+}
 
-    }
+export function healthCheck() {
+  return "OK";
+}
